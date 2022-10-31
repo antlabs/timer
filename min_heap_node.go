2a7ef1ec075713d263b5002ce937765f9b6d020c
guonaihong <guonaihong@qq.com>
2022-10-31 23:53:31 +0800

    更新

--- min_heap_node.go
+++ min_heap_node.go
@@ -1,10 +1,35 @@
 package timer
 
-import "time"
+import (
+	"time"
+)
 
 type minHeapNode struct {
-	stop       uint32
-	callback   func()
-	userExpire time.Duration
-	isSchedule bool
+	stop       uint32        //stop标记
+	callback   func()        //用户的callback
+	userExpire time.Duration //过期时间
+	isSchedule bool          //是否是周期性任务
+	index      int           //在min heap中的索引，方便删除用的
+}
+
+type nodeHeap []minHeapNode
+
+func (n nodeHeap) Len() int           { return len(n) }
+func (n nodeHeap) Less(i, j int) bool { return n[i].userExpire < n[j].userExpire }
+func (n nodeHeap) Swap(i, j int)      { n[i], n[j] = n[j], n[i] }
+
+func (n *nodeHeap) Push(x any) {
+	// Push and Pop use pointer receivers because they modify the slice's length,
+	// not just its contents.
+	*n = append(*n, x.(minHeapNode))
+	lastIndex := len(*n) - 1
+	(*n)[lastIndex].index = lastIndex
+}
+
+func (h *nodeHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
 }
