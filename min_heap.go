c9e02035ebd36b3b8912bd896554b50673e02d18
guonaihong <guonaihong@qq.com>
2024-05-15 11:17:51 +0800

    fix

--- min_heap.go
+++ min_heap.go
@@ -104,7 +104,7 @@ func (m *minHeap) getNewSleepTime() time.Duration {
 		return time.Hour
 	}
 
-	timeout := time.Since(m.minHeaps[0].absExpire)
+	timeout := m.minHeaps[0].absExpire.Sub(time.Now())
 	if timeout < 0 {
 		timeout = 0
 	}
